b7c5418b7fe030c51bf83efc70bd10e6db87dbcd
Inderpal Singh <Inderpal@ipsmac.corporate.thoughtworks.com>
2012-12-12 00:43:59 +0530

    Vinay working alone; I should get 90% share of profit GHANTA #1829

--- app/models/story.rb
+++ app/models/story.rb
@@ -11,21 +11,34 @@ class Story < ActiveRecord::Base
   alias_method_chain :commits, :sync
 
   def sync_commits repo, token
+    github_commits = []
     sha = repo.commits_without_sync.last.try(:sha)
+    params = {:access_token => token, :_owner => repo.owner, :_repo => repo.name, :per_page => 10}
     while true do
-      params = {:access_token => token, :_owner => repo.owner, :_repo => repo.name, :per_page=> 10}
-      params.merge!(:last_sha => sha) if sha
-      github_commits = Github::Commit.all(params)
-      break if github_commits.empty?
-      github_commits.each do |github_commit|
-        msg = github_commit.commit[:message]
-        committers = repo.committers.select{|committer| msg.include?(committer.name)}
-        story = Story.find_or_create_by_number(msg.match(/#[\d]*/).to_s.gsub("#",""))
-        repo.stories << story
-        repo.commits_without_sync.create(:message => msg, :committer1 => committers.first, :committer2 => committers.second, :story => story, :sha => github_commit.sha)
+      is_last_sha = false
+      commits = Github::Commit.all(params)
+      commits.each do |commit|
+        is_last_sha = true and break if (commit.sha == sha)
+        github_commits << commit
       end
-      sha = github_commits.last.sha
+      break if is_last_sha or commits.empty?
+      sha = commits.last.sha
+      params.merge!(:last_sha => sha) if sha
+    end
+    github_commits.reverse.each do |github_commit|
+      msg = github_commit.commit[:message]
+      committers = repo.committers.select { |committer| msg.include?(committer.name) }
+      story_no = extract_story_no msg
+      if story_no.nil?
+        story = Story.find_or_create_by_number(extract_story_no(msg))
+        repo.stories << story
+      end
+      repo.commits_without_sync.create(:message => msg, :committer1 => committers.first, :committer2 => committers.second, :story => story, :sha => github_commit.sha)
     end
   end
 
+  private
+  def extract_story_no(msg)
+    msg.match(/#[\d]*/).to_s.gsub("#", "")
+  end
 end
